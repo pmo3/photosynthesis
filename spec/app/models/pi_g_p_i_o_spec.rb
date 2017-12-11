12acb125190c99006b0010401726ca6a90be2f21
Patrick O'Brien <patrick@patrickmobrien.com>
2017-12-11 11:59:10 -0500

    pins

--- spec/app/models/pi_g_p_i_o_spec.rb
+++ spec/app/models/pi_g_p_i_o_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe PiGPIO, type: :model do
   it 'raises an error if ACTIVE_PINS not set' do
     expect{subject.pins}.to raise_error("You must set the active pins in app/models/pi_g_p_i_o.rb. This should be a list of pins on your pi that are connected to your lights")
   end
+
+  it 'sets the numbering to board' do
+    expect(subject.numbering).to eq(:board)
+  end
 end
